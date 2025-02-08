7d90d47c4645f340414aadd44f0e623c9b9c3ed8
artplay2006 <artemlavdar@gmail.com>
2025-02-08 13:11:40 +0300

    Update Cart.cs

--- Food_DeliveryWPF/Models/Cart.cs
+++ Food_DeliveryWPF/Models/Cart.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Food_DeliveryWPF.Models
 {
+    // корзина
     public static class Cart
     {
         public static List<ContentOrder>dishes = new List<ContentOrder>();
